c565df179bc1650d50be3d36a9abdaf12cf25c45
Luis Arturo García Gonzalez <lgarcia103@alumnos.uaq.mx>
2020-12-22 20:42:16 -0600

    Weird SVG load problem commit AKA wtf

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
-	import { Router, Route } from "svelte-navigator";
+	import { Router, Link, Route } from "svelte-navigator";
+	import Switch from "svelte-switch";
 	import Home from "./routes/Home.svelte";
+
+	let checkedValue = true;
+
+	function handleChange(e) {
+        const { checked } = e.detail;
+        checkedValue = checked;
+        console.log(checkedValue);
+    }
 </script>
 
 <style type="text/scss">
@@ -20,6 +29,42 @@
 		font-family: 'Fira Code', monospace;
 		background-color: $background-light;
 	}
+
+    :global(.nav-item){
+        text-decoration: none;
+        color: $textColor-light;
+        padding: 2px 10px;
+        box-sizing: border-box;
+        border-radius: 20px;
+    }
+
+    :global(.nav-item):visited{
+        color: inherit;
+    }
+
+    :global(.nav-item):hover{
+        color: $accent-color;
+        text-decoration: none;
+    }
+
+	.navbar {
+        position: absolute;
+        right: 0;
+        box-sizing: border-box;
+        margin-top: 30px;
+        margin-right: 60px;
+        padding: 10px;
+        width: 500px;
+        height: 50px;
+        display: flex;
+        border-radius: 30px;
+        border: #DDDDDD solid 2px;
+        align-items: center;
+        justify-content: space-around;
+        background-color: $background-light;
+        z-index: 1;
+    }
+
 </style>
 
 <svelte:head>
@@ -28,5 +73,24 @@
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap" rel="stylesheet">
 </svelte:head>
 <Router>
+	<div class="navbar">
+        <Link class="nav-item" to="#">Home</Link>
+        <Link class="nav-item" to="#">Portfolio</Link>
+        <Link class="nav-item" to="#">Blog</Link>
+        <Link class="nav-item" to="#">Contact</Link>
+        <Switch 
+            offColor="#2B303F"
+            offHandleColor="#8C50FF"
+            onHandleColor="#8C50FF"
+            onColor="#DDD"
+            unCheckedIcon={false}
+            checked={false}
+            handleDiameter={""}
+            on:change={handleChange}
+        >
+            <span slot="checkedIcon" />
+            <span slot="unCheckedIcon" />
+        </Switch>
+    </div>
 	<Route path="/" component={Home} primary={false}/>
 </Router>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,58 +1,22 @@
 <script>
-    import Switch from "svelte-switch";
     import { Link } from "svelte-navigator";
-    // import AppSvg from "../img/app_development.svg"
+    import DarkMode from "svelte-dark-mode";
+    import DevSvg from "../img/landing_image.svelte";
 
-    let checkedValue = true;
-    let location;
-    let navigate;
+    let theme;
 
-    function handleChange(e) {
-    const { checked } = e.detail;
-    checkedValue = checked;
-    }
+    $: switchTheme = theme === "dark" ? "light" : "dark";
+    $: document.body.className = theme; // "dark" or "light"
+
+    export let location;
+    export let navigate;
 </script>
 
 <style type="text/scss">
     @import '../styles/vars';
-    :global(.nav-item){
-        text-decoration: none;
-        color: $textColor-light;
-        padding: 2px 10px;
-        box-sizing: border-box;
-        border-radius: 20px;
-    }
-
-    :global(.nav-item):visited{
-        color: inherit;
-    }
-
-    :global(.nav-item):hover{
-        color: $accent-color;
-        text-decoration: none;
-    }
-
     .app {
         height: 100%;
         width: 100%;
-    }
-
-    .navbar {
-        position: absolute;
-        right: 0;
-        box-sizing: border-box;
-        margin-top: 30px;
-        margin-right: 60px;
-        padding: 10px;
-        width: 500px;
-        height: 50px;
-        display: flex;
-        border-radius: 30px;
-        border: #DDDDDD solid 2px;
-        align-items: center;
-        justify-content: space-around;
-        background-color: $background-light;
-        z-index: 1;
     }
 
     .background {
@@ -119,7 +83,7 @@
             text-decoration: none;
         }
 
-    .image {
+    #image {
         width: 45%;
         display: flex;
         justify-content: center;
@@ -129,24 +93,7 @@
 </style>
 
 <main class="app">
-    <div class="navbar">
-        <Link class="nav-item" to="#">Home</Link>
-        <Link class="nav-item" to="#">Portfolio</Link>
-        <Link class="nav-item" to="#">Blog</Link>
-        <Link class="nav-item" to="#">Contact</Link>
-        <Switch 
-            offColor="#2B303F"
-            offHandleColor="#8C50FF"
-            onHandleColor="#8C50FF"
-            onColor="#F0F0F0"
-            unCheckedIcon={false}
-            checked={false}
-            handleDiameter={""}
-        >
-            <span slot="checkedIcon" />
-            <span slot="unCheckedIcon" />
-        </Switch>
-    </div>
+    <DarkMode bind:theme />
 
     <div class="background"></div>
 
@@ -156,8 +103,8 @@
             <p class="dev-text" >I'm a Full-Stack Developer</p>
             <Link class="portfolio-button" to="#">Watch my work!</Link>
         </div>
-        <div class="image">
-            <!-- <img src={AppSvg} alt="Developer" width="80%"> -->
+        <div id="image">
+            <DevSvg/>
         </div>
     </div>
 </main>
