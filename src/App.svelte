bc2c9f35d3039bab055d44ea9c1fe24ba1488470
Luis Arturo García González <lgarcia103@alumnos.uaq.mx>
2021-08-04 17:40:41 -0500

    feat: Home page re-design before adding skills and projects

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,11 @@
 <script>
 	import { Router, Link, Route } from "svelte-navigator";
 	import Switch from "svelte-switch";
-    import Home from "./routes/Home.svelte";
+    import Home from "./pages/Home.svelte";
     import DarkMode from "svelte-dark-mode";
 
     let theme;
 
-    $: switchTheme = theme === "dark" ? "light" : "dark";
     $: checkedValue = theme === "dark" ? true : false;
     $: document.body.className = theme; // "dark" or "light"
 
@@ -22,19 +21,19 @@
 <style type="text/scss">
 
     :global(body.dark) {
-        --bg: #323239;
+        --bg: #272727;
         --color: white;
-        --accent: #8C50FF;
+        --accent: #F4CE2C;
         --border: #747474;
-        --secondary: #2EB0D9;
+        --secondary: #FF2424;
     }
 
     :global(body.light) {
         --bg: #F8F8F8;
-        --color: #2B303F;
-        --accent: #8C50FF;
-        --border: #DDDDDD;
-        --secondary: #2EB0D9;
+        --color: #353535;
+        --accent: #f4cf2c;
+        --border: #e4e4e4;
+        --secondary: #FF2424;
     }
 
 	:global(html) {
@@ -48,8 +47,10 @@
 		width: 100%;
 		margin: 0;
 		padding: 0;
-		font-family: 'Fira Code', monospace;
+		font-family: 'Poppins', sans-serif;
 		background-color: var(--bg);
+        overflow-x: hidden;
+        box-sizing: border-box;
 	}
 
     :global(.nav-item){
@@ -58,6 +59,7 @@
         padding: 2px 10px;
         box-sizing: border-box;
         border-radius: 20px;
+        margin-right: 1.5rem;
     }
 
     :global(.nav-item):visited{
@@ -65,57 +67,87 @@
     }
 
     :global(.nav-item):hover{
-        color: var(--accent);
+        color: var(--secondary);
         text-decoration: none;
+    }
+    
+    :global(.nav-section){
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
     }
 
 	.navbar {
-        position: absolute;
-        right: 0;
         box-sizing: border-box;
-        margin-top: 30px;
-        margin-right: 60px;
-        padding: 10px;
-        width: 500px;
-        height: 50px;
+        padding: 0 3rem;
+        width: 100%;
+        height: 4rem;
         display: flex;
-        border-radius: 30px;
-        border: #DDDDDD solid 2px;
         align-items: center;
-        justify-content: space-around;
+        font-weight: 600;
+        font-size: 15px;
+        justify-content: space-between;
         background-color: var(--bg);
         color: var(--color);
-        border-color: var(--border);
-        z-index: 1;
+    }
+
+    :global(.nav-icon){
+        text-decoration: none;
+        color: var(--color) !important;
+        font-size: 1.5rem;
+        padding: 0 0.5rem;
+        border-radius: 5px;
+    }
+
+    :global(.nav-icon):hover{
+        background-color: var(--border);
+    }
+
+
+    .spacing {
+        display: inline-flex;
+        gap: 1.5rem;
     }
 
 </style>
 
 <svelte:head>
-	<title>Welcome to my Portfolio</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap" rel="stylesheet">
+	<title>Luis Arturo | Web Developer</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/0f61084166.js" crossorigin="anonymous"></script>
 </svelte:head>
 <Router>
     <DarkMode bind:theme />
 	<div class="navbar">
-        <Link class="nav-item" to="#">Home</Link>
-        <Link class="nav-item" to="#">Portfolio</Link>
-        <Link class="nav-item" to="#">Blog</Link>
-        <Link class="nav-item" to="#">Contact</Link>
-        <Switch 
-            offColor="#2B303F"
-            offHandleColor="#8C50FF"
-            onHandleColor="#8C50FF"
-            onColor="#DDD"
-            unCheckedIcon={false}
-            checked={checkedValue}
-            handleDiameter={""}
-            on:change={handleChange}
-        >
-            <span slot="checkedIcon" />
-            <span slot="unCheckedIcon" />
-        </Switch>
+        <div class="nav-section spacing">
+            <a href="https://www.linkedin.com/in/luis-arturo" class="nav-icon" target="_blank">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/Kingarturs" class="nav-icon" target="_blank">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+        <div class="nav-section">
+            <Link class="nav-item" to="#">Home</Link>
+            <Link class="nav-item" to="#">Portfolio</Link>
+            <Link class="nav-item" to="#">Contact</Link>
+            <Switch 
+                offColor="#2B303F"
+                offHandleColor="#FF2424"
+                onHandleColor="#FF2424"
+                onColor="#DDD"
+                unCheckedIcon={false}
+                checked={checkedValue}
+                handleDiameter={""}
+                on:change={handleChange}
+            >
+                <span slot="checkedIcon" />
+                <span slot="unCheckedIcon" />
+            </Switch>
+        </div>
     </div>
+
 	<Route path="/" component={Home} primary={false}/>
 </Router>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,139 @@
+<script>
+    import { Link } from "svelte-navigator";
+    import { fly } from 'svelte/transition';
+    const src = "../images/app_development.svg";
+
+    export let location;
+    export let navigate;
+</script>
+
+<style type="text/scss">
+    .app {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        overflow-y: auto;
+    }
+
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    .app::-webkit-scrollbar {
+        display: none;
+    }
+
+    /* Hide scrollbar for IE, Edge and Firefox */
+    .app {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+    }
+
+    /* .background {
+        position: absolute;
+        top:0;
+        left:0;
+        right:0;
+        bottom:0;
+        height: 100%;
+        width: 100%;
+        -webkit-clip-path: polygon(55% 0, 100% 0, 100% 100%, 80% 100%);
+        clip-path: polygon(55% 0, 100% 0, 100% 100%, 80% 100%);
+        background: linear-gradient(10deg, var(--accent), var(--secondary));
+        z-index: -1;
+    } */
+
+    .splash-screen {
+        padding: 0 3rem;
+        box-sizing: border-box;
+        width: 100%;
+        height: calc(100vh - 4rem);
+        display: flex;
+        flex-direction: row;
+        flex-shrink: 0;
+        position: relative;
+        z-index: 0;
+    }
+
+    .skills {
+        padding: 0px 4rem;
+        box-sizing: border-box;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        flex-direction: row;
+        flex-shrink: 0;
+        position: relative;
+        z-index: 0;
+    }
+
+    .text {
+        width: 55%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        color: var(--color);
+    }
+        .name-text {
+            font-size: 2rem;
+            color: var(--color);
+            margin: 0;
+        }
+
+        .dev-text {
+            font-size: 3.8rem;
+            color: var(--secondary);
+            font-weight: bold;
+            margin: 0;
+            margin-top: 15px;
+            line-height: 1.2;
+        }
+
+        :global(.portfolio-button){
+            color: var(--secondary);
+            background-color: var(--bg);
+            text-decoration: none;
+            font-weight: bold;
+            font-size: 16px;
+            border-radius: 10px;
+            border: solid 2px var(--secondary);
+            padding: 10px 20px;
+            box-sizing: border-box;
+            margin-top: 15px;
+            transition: padding .1s;
+            transition-timing-function: ease;
+        }
+
+        :global(.portfolio-button):visited{
+            color: var(--secondary);
+        }
+
+        :global(.portfolio-button):hover{
+            padding: 10px 36px;
+            text-decoration: none;
+        }
+
+    #image {
+        width: 45%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
+
+<main class="app">
+    <div class="splash-screen">
+        <!-- <div in:fly="{{duration: 400, x: 700}}" class="background" /> -->
+        <div class="text">
+            <p class="name-text" >Hi! My name's Luis!</p>
+            <p class="dev-text" >I'm a Full-Stack Developer</p>
+            <Link class="portfolio-button" to="#">Watch my work</Link>
+        </div>
+        
+        <div id="image">
+            <img width="85%" src={src} alt="Animation">
+        </div>
+    </div>
+    <!-- <div class="skills">
+    </div> -->
+</main>
